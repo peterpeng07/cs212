0edf5bff7a2e89e048478acbc380baec24548f1b
peterpeng07 <72902547+peterpeng07@users.noreply.github.com>
2021-02-12 10:26:27 -0500

    project1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿/* Program 1: Lg Lg n
+ * Peter Peng, cp47
+ * CS-212-B, Professor Plantinga, 9/2020
+ */
+using System;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // title
+            Console.WriteLine("Floor (lg lg n) Computation");
+     
+            while (true)
+            {
+                // prompt user to enter number
+                Console.Write("\nEnter n: ");
+                float n = float.Parse(Console.ReadLine());
+                // output calculated result
+                Console.WriteLine("Floor (lg lg " + n + ") = " + Lg(Lg(n)));
+            }
+        }
+        static long Lg(float n)
+        {
+            int count = 0;
+            // devide n by 2 until n is smaller than 2
+            // count the divisions
+            while (n >= 2)
+            {
+                n = n / 2;
+                count++;
+            }
+            return count;
+        }
+    }
+}
